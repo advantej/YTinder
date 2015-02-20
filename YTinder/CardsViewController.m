28c82a85eb8d23056d34250e65a32ce6115f55d2
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-02-19 20:33:34 -0800

    trick to move one image form one vc to other

--- YTinder/CardsViewController.m
+++ YTinder/CardsViewController.m
@@ -49,23 +49,38 @@
     UIViewController *toController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     UIViewController *fromController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
 
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:self.cardImageView.image];
+    imageView.frame = self.cardImageView.frame;
+    imageView.contentMode = self.cardImageView.contentMode;
+    imageView.clipsToBounds = self.cardImageView.clipsToBounds;
+
+    [[[UIApplication sharedApplication] keyWindow] addSubview:imageView];
     if (self.isPresenting) {
+
+        [self.cardImageView setHidden:YES];
+
         NSLog(@"I'm presenting"); //i'm from controller
         toController.view.frame = fromController.view.frame;
         [containerView addSubview:toController.view];
         toController.view.alpha = 0;
         [UIView animateWithDuration:0.4 animations:^{
             toController.view.alpha = 1;
+            imageView.transform = CGAffineTransformScale(imageView.transform, 1.1, 1.1);
         } completion:^(BOOL finished) {
             [transitionContext completeTransition:YES];
+            [imageView removeFromSuperview];
         }];
     } else {
         NSLog(@"I'm dismissing"); // i'm toController
         [UIView animateWithDuration:0.4 animations:^{
+            [((ProfileViewController *) fromController).profileImage setHidden:YES];
             fromController.view.alpha = 0;
+            imageView.transform = CGAffineTransformScale(imageView.transform, 1, 1);
         } completion:^(BOOL finished) {
             [transitionContext completeTransition:YES];
             [fromController.view removeFromSuperview];
+            [imageView removeFromSuperview];
+            [self.cardImageView setHidden:NO];
         }];
     }
 
